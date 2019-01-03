e25492820c5753c736fa7cdd378e311917599213
vacrozet <vacrozet@student.42.fr>
2019-01-03 20:52:19 +0100

    3/1/19-21:52:17

---
+++ wifi-sncf-inoui.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+alldnsserver=$(cat /etc/resolv.conf| grep "nameserver" | cut -c12- | tr "\n" " " | rev | cut -c1- | rev)
+newsdns=$(route -n get default | grep gateway | cut -c14-)
+sudo networksetup -setdnsservers Wi-Fi ${alldnsserver} ${newsdns}
+sleep 5
+open -a /Applications/Google\ Chrome.app -n --args 'https://wifi.sncf/'
