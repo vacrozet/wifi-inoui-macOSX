df91914f6b0a96ef33479a1c1b01aaa81cbf572f
Tac&Tic <vacrozet@student.42.fr>
2019-01-07 06:39:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cd ~/
 git clone https://github.com/vacrozet/wifi-inoui-macOSX.git .wifi-sncf-inoui
-echo 'alias wifi-sncf="sh ~/.wifi-sncf-inoui/.wifi-sncf-inoui.sh"' >> ~/.zshrc
+echo 'alias wifi-sncf="sh ~/.wifi-sncf-inoui/wifi-sncf-inoui.sh"' >> ~/.zshrc
 sleep 5
 source ~/.zshrc
