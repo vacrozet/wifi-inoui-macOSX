df6c103c442458e7541515af5c17b4b88549ffd9
Tac&Tic <vacrozet@student.42.fr>
2019-01-07 06:38:54 +0100

    Update wifi-sncf-inoui.sh

--- wifi-sncf-inoui.sh
+++ wifi-sncf-inoui.sh
@@ -3,5 +3,5 @@
 alldnsserver=$(cat /etc/resolv.conf| grep "nameserver" | cut -c12- | tr "\n" " " | rev | cut -c1- | rev)
 newsdns=$(route -n get default | grep gateway | cut -c14-)
 sudo networksetup -setdnsservers Wi-Fi ${alldnsserver} ${newsdns}
-sleep 5
+sleep 2
 open -a /Applications/Google\ Chrome.app -n --args 'https://wifi.sncf/'
