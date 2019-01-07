ede5a4234ecfd12cc46af9b2334c2ddcff30d670
vacrozet <vacrozet@student.42.fr>
2019-01-07 06:58:47 +0100

    change command

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 cd ~/
+cmd=${alias='alias wifi-sncf="sh ~/.wifi-sncf-inoui/wifi-sncf-inoui.sh"'}
 git clone https://github.com/vacrozet/wifi-inoui-macOSX.git .wifi-sncf-inoui
-echo 'alias wifi-sncf="sh ~/.wifi-sncf-inoui/wifi-sncf-inoui.sh"' >> ~/.zshrc
-sleep 5
+sleep 2
+echo $cmd >> ~/.zshrc
 source ~/.zshrc
